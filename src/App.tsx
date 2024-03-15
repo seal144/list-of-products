b9960ac6be32fdafe3fa8d6814a537393b5bd139
RafalFak <rafalfak8@gmail.com>
2024-03-15 14:49:07 +0100

    docs(App): add some description about task requirements implementation

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,19 @@ import Layout from './layouts/Layout';
 import Home from './views/Home';
 import ProductsDataProvider from './context/ProductsDataContext';
 
+// BASIC TASK REQUIREMENTS IMPLEMENTED:
+// - table with products (5 items per page) - ProductsTable.tsx
+// - pagination and search by id input (only numbers) - ProductsTable.tsx
+// - modal with all items data - ProductModal.tsx
+// - displaying proper error message for user if endpoint returns 4XX, 5XX - ProductsTable.tsx
+// - filtering and pagination is performed within the API
+// OPTIONAL TASK REQUIREMENTS IMPLEMENTED:
+// - pagination and filtering is reflected in the URL - so user can share specific data
+// - unit tests - productsApi.test.js and parseKey.test.js
+// - global state handled with Context - ProductsDataContext.tsx
+// - debouncing used - custom for "Search by ID" feature in ProductsTable.tsx
+// TECH STACK: React, Tanstack/react-query, Material UI, Testing library
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
